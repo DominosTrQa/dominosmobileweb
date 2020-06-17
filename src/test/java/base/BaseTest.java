a8054cb631d2dcca957aa6126e40e93d6d031b23
testinium <emre.sonmez@testinium.com>
2020-06-17 10:14:10 +0300

    smoke live hatalar giderildi

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -45,10 +45,10 @@ public class BaseTest {
                 capabilities.setCapability(MobileCapabilityType.BROWSER_NAME, BrowserType.CHROME);
                 capabilities.setCapability(MobileCapabilityType.PLATFORM, Platform.ANDROID);
                 capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-                capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Galaxy A30");
+                capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "G5");
                 //capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "uiautomator2");
                 //capabilities.setCapability(MobileCapabilityType.UDID, "R58M40CLP8A");
-                capabilities.setCapability(MobileCapabilityType.VERSION, "9.0");
+                capabilities.setCapability(MobileCapabilityType.VERSION, "7.0");
 
                 URL url = new URL("http://127.0.0.1:4723/wd/hub");
                 webDriver = new AndroidDriver(url,capabilities);
